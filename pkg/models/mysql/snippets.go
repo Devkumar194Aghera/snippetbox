944a65f01ba9852f3f89dab3873d42b1966569a3
Dev Aghera <devaghera194@gmail.com>
2024-11-08 17:09:49 +0530

    Created basic snippet box for text storage with last feature till now : session management for new snippet flash message

---
+++ pkg/models/mysql/snippets.go
@@ -0,0 +1,85 @@
+package mysql
+
+import (
+	"database/sql"
+	"goModule/snippetbox/pkg/models"
+)
+
+// Define a SnippetModel type which wraps a sql.DB connection pool.
+type SnippetModel struct {
+	DB *sql.DB
+}
+
+// This will insert a new snippet into the database.
+func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
+
+	stmt := `INSERT INTO snippets (title, content, created, expires)
+			VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	result, err := m.DB.Exec(stmt, title, content, expires)
+
+	if err != nil {
+		return 0, nil
+	}
+
+	id, err := result.LastInsertId()
+
+	if err != nil {
+		return 0, nil
+	}
+
+	return int(id), nil
+}
+
+// This will return a specific snippet based on its id.
+func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
+
+	stmt := `select id, title, content, created, expires from snippets 
+			where expires > UTC_TIMESTAMP() and id = ?`
+
+	row := m.DB.QueryRow(stmt, id)
+
+	s := &models.Snippet{}
+
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+
+	if err == sql.ErrNoRows {
+		return nil, models.ErrNoRecord
+	} else if err != nil {
+		return nil, err
+	}
+
+	return s, nil
+}
+
+// This will return the 10 most recently created snippets.
+func (m *SnippetModel) Latest() ([]*models.Snippet, error) {
+
+	stmt := `select id, title, content, created, expires from snippets 
+			where expires > UTC_TIMESTAMP() order by created desc LIMIT 10 `
+
+	rows, err := m.DB.Query(stmt)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	snippets := []*models.Snippet{}
+
+	for rows.Next() {
+
+		s := &models.Snippet{}
+
+		err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+
+		if err != nil {
+			return nil, err
+		}
+
+		snippets = append(snippets, s)
+	}
+
+	return snippets, nil
+}
